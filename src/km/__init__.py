04a3f20fd503d70d2fb74afc4dbefae0fd8bfbe4
Isaac A. Nugroho <isaac.nugroho@gmail.com>
2026-06-15 15:41:56 +0700

    bump to v0.5.2

--- src/km/__init__.py
+++ src/km/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("km")
 except PackageNotFoundError:
-    __version__ = "0.5.1"
+    __version__ = "0.5.2"
